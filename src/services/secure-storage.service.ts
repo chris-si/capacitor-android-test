133fcbb546cbfbf475b4cd8bb135d2c7f711655d
Chris/si <24972677+chris-si@users.noreply.github.com>
2024-07-19 09:12:59 +0200

    use different sec storage plugin for android platform

--- src/services/secure-storage.service.ts
+++ src/services/secure-storage.service.ts
@@ -6,18 +6,32 @@ import {
   StorageError,
 } from "@aparajita/capacitor-secure-storage";
 import { Capacitor } from "@capacitor/core";
+import {
+  SecureStoragePlugin as SecureStoragePluginAndroid,
+  SecureStoragePluginPlugin as SecureStoragePluginPluginAndroid,
+} from "capacitor-secure-storage-plugin";
 
 type StorageKeys = "someToken";
 
 export class SecureStorageService {
   private static _instance: SecureStorageService;
   private readonly logger = new Logger(SecureStorageService.name);
+  private _platform: string;
+  private _isPlatformAndroid: boolean;
   private _storage: SecureStoragePlugin;
+  private _storageAndroid: SecureStoragePluginPluginAndroid;
 
   private readonly KEY_PREFIX = "my-custom-prefix_";
 
   private constructor() {
-    this._storage = SecureStorage;
+    this._platform = Capacitor.getPlatform();
+    this._isPlatformAndroid = this._platform === "android";
+
+    if (this._isPlatformAndroid) {
+      this._storageAndroid = SecureStoragePluginAndroid;
+    } else {
+      this._storage = SecureStorage;
+    }
   }
 
   public static async getInstance(): Promise<SecureStorageService> {
@@ -32,38 +46,72 @@ export class SecureStorageService {
 
   private async init(): Promise<void> {
     this.logger.log("init start");
-    await this._storage.setKeyPrefix(this.KEY_PREFIX + "_");
-    this.logger.log("setKeyPrefix done");
+    if (this._isPlatformAndroid) {
+      this.logger.log("using Android storage plugin");
+    } else {
+      await this._storage.setKeyPrefix(this.KEY_PREFIX + "_");
+      this.logger.log("setKeyPrefix done");
+    }
 
-    const platform = Capacitor.getPlatform();
-    if (platform === "ios") {
+    if (this._platform === "ios") {
       await this._storage.setSynchronize(false);
     }
   }
 
   private async set(
     key: StorageKeys,
-    value: DataType,
+    value: string,
     sync = false
   ): Promise<void> {
-    return this._storage.set(key, value, false, sync);
+    if (this._isPlatformAndroid) {
+      await this._storageAndroid.set({
+        key: key,
+        value: value,
+      });
+    } else {
+      return this._storage.set(key, value, false, sync);
+    }
   }
 
-  private async get(key: string): Promise<DataType | null> {
-    return this._storage.get(key);
+  private async get(key: string): Promise<DataType | null | string> {
+    if (this._isPlatformAndroid) {
+      try {
+        const res = await this._storageAndroid.get({ key: key });
+        return res.value;
+      } catch (e) {
+        this.logger.log(e);
+        return null;
+      }
+    } else {
+      return this._storage.get(key);
+    }
   }
 
   private async remove(key: string, sync = false): Promise<boolean> {
-    return this._storage.remove(key, sync);
+    if (this._isPlatformAndroid) {
+      try {
+        await this._storageAndroid.remove({ key: key });
+        return true;
+      } catch (e) {
+        this.logger.log(e);
+        return false;
+      }
+    } else {
+      return this._storage.remove(key, sync);
+    }
   }
 
   private async clear(sync = false): Promise<void> {
-    return this._storage.clear(sync);
+    if (this._isPlatformAndroid) {
+      await this._storageAndroid.clear();
+    } else {
+      return this._storage.clear(sync);
+    }
   }
 
   private defaultCrud(key: StorageKeys, value?: DataType, sync = false) {
     return {
-      set: async (data: DataType) => {
+      set: async (data: string) => {
         return this.set(key, data);
       },
       get: async <T extends DataType>() => {
